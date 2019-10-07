aa465766410fb66d90525c2655905b85b83ad6de
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-10-07 16:31:51 -0500

    all crud and error handling
    Co-authored by Moses Samuel <mosamuel98@gmail.com>
    Co-authored by Chris Bonifacio <christopher.bonifacio@gmail.com>

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
-
 const db = require('./data/dbConfig.js');
+const accountsRouter = require('./accounts/accounts')
 
 const server = express();
 
 server.use(express.json());
+server.use('/api/accounts', accountsRouter)
 
 module.exports = server;
